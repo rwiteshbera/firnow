59fd5485646c015c2d20557bd45f16fcc07f590c
Tsubasa <scientistjishan@gmail.com>
2023-10-09 23:02:12 +0530

    Added police station table.

---
+++ models/tables.py
@@ -0,0 +1,21 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class PoliceStation(Model):
+    id = fields.BigIntField(pk=True, index=True)
+    email = fields.CharField(max_length=256, unique=True, index=True)
+    password = fields.CharField(max_length=300)
+    state = fields.CharField(max_length=256)
+    district = fields.CharField(max_length=256)
+    name = fields.CharField(max_length=256)
+    verified = fields.BooleanField(default=False)
+    updated_at = fields.DatetimeField(auto_now=True, alias="updatedAt")
+
+    class Meta:
+        table = "police_stations"
+        table_description = "Police Station Details"
+        ordering = ["state", "district", "name"]
+
+    def __str__(self) -> str:
+        return self.name
