c013bb551eaf874d087f065f452a57b5f89f9dfa
Tsubasa <scientistjishan@gmail.com>
2023-10-10 21:55:43 +0530

    Added new file, listing all police station URLs for reuse.

---
+++ routes/police_station_urls.py
@@ -0,0 +1,13 @@
+from pydantic import HttpUrl
+
+from config import settings
+
+prefix = "police-station"
+
+HOST: HttpUrl = settings.APP_HOST
+LOGIN_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/login")
+REGISTER_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/register")
+REFRESH_TOKEN_URL: HttpUrl = HttpUrl(f"{HOST}auth/refresh")
+SEND_OTP_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/send-otp")
+VERIFY_EMAIL_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/verify-email")
+POLICE_STATION_DASHBOARD_URL: HttpUrl = HttpUrl(f"{HOST}/{prefix}/dashboard")
