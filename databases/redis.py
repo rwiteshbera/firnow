4f2daa7ddf4c8a98c2bb875fbe265a34b60b5cf8
Tsubasa <scientistjishan@gmail.com>
2023-10-10 21:53:37 +0530

    Updated redis to use new settings.

--- databases/redis.py
+++ databases/redis.py
@@ -1,26 +1,30 @@
+import asyncio
 from typing import Optional
 
 import redis.asyncio as redis
 
-from settings import config
+from config import settings
 
 
 class RedisClient:
     _db: Optional[redis.Redis] = None
-    _host: str = config["REDIS_HOST"] or "localhost"
-    _port: int = int(config["REDIS_PORT"] or 6379)
-    _password: Optional[str] = config["REDIS_PASSWORD"]
 
     @classmethod
-    def get_client(cls) -> redis.Redis:
+    async def get_client(cls) -> redis.Redis:
         if cls._db is None:
-            cls._db = redis.Redis(
-                host=cls._host,
-                port=cls._port,
-                password=cls._password,
+            # cls._db = redis.Redis(
+            #     host=cls._host,
+            #     port=cls._port,
+            #     password=cls._password,
+            #     protocol=3,
+            #     decode_responses=True,
+            # )
+            cls._db = await redis.from_url(
+                str(settings.REDIS_URL),
                 protocol=3,
                 decode_responses=True,
             )
+
         return cls._db
 
     @classmethod
