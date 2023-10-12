c4740ff8a8a9e6e70d8c1da70cde9c652136bf46
Tsubasa <scientistjishan@gmail.com>
2023-10-12 23:58:19 +0530

    Added new error models.

--- models/errors.py
+++ models/errors.py
@@ -1,8 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, HttpUrl
+
+
+class ErrorMessage(BaseModel):
+    message: str
+
+
+class ErrorMessageWithRedirect(ErrorMessage):
+    redirect: HttpUrl
+
+
+class ErrorMessageWithAction(ErrorMessage):
+    action: HttpUrl
 
 
 class RequestError(BaseModel):
-    detail: str
+    detail: ErrorMessage
+
+
+class RequestErrorWithRedirect(BaseModel):
+    detail: ErrorMessageWithRedirect
+
+
+class RequestErrorWithAction(BaseModel):
+    detail: ErrorMessageWithAction
 
 
 class PoliceStationNotFound(Exception):
