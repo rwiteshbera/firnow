bd6a881a58e38405c8be2a8e18ba7581a6b7d5f3
Tsubasa <scientistjishan@gmail.com>
2023-10-13 19:04:12 +0530

    Added endpoint to refresh access tokens.

--- services/auth.py
+++ services/auth.py
@@ -1,9 +1,43 @@
-from fastapi import FastAPI
+import asyncio
+from typing import Annotated
 
+from fastapi import Depends, FastAPI, Response, status
+
+from models.auth import AccessToken
+from models.errors import RequestErrorWithRedirect
 from routes import police_station
+from utils.dependencies import get_id_from_token
+from utils.token import get_access_token_obj
 
 auth_service = FastAPI()
 auth_service.include_router(police_station.router)
+
+
+@auth_service.get(
+    "/refresh",
+    responses={
+        status.HTTP_200_OK: {
+            "model": AccessToken,
+        },
+        status.HTTP_400_BAD_REQUEST: {
+            "model": RequestErrorWithRedirect,
+        },
+        status.HTTP_401_UNAUTHORIZED: {
+            "model": RequestErrorWithRedirect,
+        },
+    },
+    tags=["General"],
+    summary="Refresh access token",
+)
+async def refresh_token(
+    response: Response, id: Annotated[int, Depends(get_id_from_token)]
+):
+    """
+    This endpoint is used to refresh the access token.
+    It requires `refresh_token` cookie to be set.
+    """
+    access_token_obj = await get_access_token_obj(id, response)
+    return access_token_obj
 
 
 if __name__ == "__main__":
