17e2d362cf65acea7dfae32c344fc71cce4d0498
Tsubasa <scientistjishan@gmail.com>
2023-10-15 10:29:14 +0530

    Removed unused dependencies and refactored code.

--- main.py
+++ main.py
@@ -1,38 +1,18 @@
-from contextlib import asynccontextmanager
-
 import uvicorn
 from fastapi import FastAPI, status
 from fastapi.exceptions import HTTPException
 from tortoise import Tortoise
 from tortoise.exceptions import DoesNotExist
 
-from config import settings
-from databases.redis import RedisClient
 from models.errors import RequestError
 from models.police_station import PoliceStation_Pydantic
 from models.tables import PoliceStation
 from services.auth import auth_service
 from services.id import id_service
 from services.location import location_service
-from session import SingletonSession
+from session import manage_session
 
-
-@asynccontextmanager
-async def manage_session(app: FastAPI):
-    await RedisClient.get_client()
-    SingletonSession.get_session()
-    await Tortoise.init(
-        db_url=str(settings.POSTGRES_URL),
-        modules={"models": ["models.tables"]},
-        use_tz=True,
-    )
-    await Tortoise.generate_schemas()
-    yield
-    await SingletonSession.close_session()
-    await Tortoise.close_connections()
-
-
-app = FastAPI(lifespan=manage_session)
+app = FastAPI(lifespan=manage_session, prefix="/main")
 
 
 @app.get(
