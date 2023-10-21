89b9504bd750754dd1365ea05f28f910eb04cb4d
Tsubasa <scientistjishan@gmail.com>
2023-10-22 00:39:58 +0530

    Added parsing logic for web3 storage token and a new variable project mode.

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
 from datetime import datetime
+from enum import Enum
 from pathlib import Path
 
 from pydantic import FilePath, HttpUrl, PostgresDsn, RedisDsn, field_validator
 from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Mode(Enum):
+    DEV = "dev"
+    PROD = "prod"
 
 
 class Settings(BaseSettings):
@@ -17,12 +23,14 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str = (
         "37401a016623f5f320bda74c83063d32ebc4bf5417bd5dbf99aa4f0afbf4cb02"
     )
+    MODE: Mode = Mode.DEV
     NODE_ID: int = 0
     POSTGRES_URL: PostgresDsn = PostgresDsn(
         "postgresql://postgres:postgres@localhost:5432/postgres"
     )
     REDIS_URL: RedisDsn = RedisDsn("redis://username:password@localhost:6379")
     REFRESH_TOKEN_EXPIRE_HOURS: int = 72
+    WEB3_STORAGE_TOKEN: str = ""
 
     @field_validator("APP_CREATION_DATE", mode="before")
     @classmethod
@@ -35,6 +43,13 @@ class Settings(BaseSettings):
 
         except ValueError:
             raise ValueError("Invalid date format. Please use DD-MM-YYYY format.")
+
+    @field_validator("WEB3_STORAGE_TOKEN", mode="before")
+    @classmethod
+    def parse_web3_storage_token(cls, value: str) -> str:
+        if value:
+            return value
+        raise ValueError("WEB3_STORAGE_TOKEN is required.")
 
 
 settings = Settings()
