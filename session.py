d9a5148ddf5423d8e67bb17b21316ab8b93a90b5
Tsubasa <scientistjishan@gmail.com>
2023-10-24 20:53:05 +0530

    Initialized mailer client.

--- session.py
+++ session.py
@@ -7,6 +7,7 @@ from tortoise import Tortoise
 
 from config import settings
 from databases.redis import RedisClient
+from utils.mail import Mailer
 
 
 class SingletonSession:
@@ -26,16 +27,18 @@ class SingletonSession:
 
 
 @asynccontextmanager
-async def manage_sessions(app: FastAPI):
-    await RedisClient.get_client()
+async def init(app: FastAPI):
     SingletonSession.get_session()
+    await RedisClient.get_client()
     await Tortoise.init(
         db_url=str(settings.POSTGRES_URL),
         modules={"models": ["models.tables"]},
         use_tz=True,
     )
     await Tortoise.generate_schemas()
+    await Mailer.get_client()
     yield
     await SingletonSession.close_session()
     await Tortoise.close_connections()
     await RedisClient.close_client()
+    await Mailer.close_client()
