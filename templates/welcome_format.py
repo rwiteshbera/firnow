84842720b53460812902a94d0c9a3f72f4ceb603
Tsubasa <scientistjishan@gmail.com>
2023-10-24 21:03:19 +0530

    Added template for welcome message.

---
+++ templates/welcome_format.py
@@ -0,0 +1,46 @@
+welcome_html = """\
+<html>
+  <head>
+    <style>
+      body {
+        font-size: 16px;
+      }
+      p {
+        font-size: 1rem;
+      }
+    </style>
+  </head>
+  <body>
+    <p>
+      Welcome to FIRNow!
+    </p>
+
+    <p>
+      Your police station is now registered, and you have entered a new era of 
+      streamlined FIR management. With our user-friendly system, you can efficiently 
+      handle, store, and track FIRs, ensuring a more organized and responsive approach 
+      to law enforcement.
+    </p>
+
+    <p>
+      If you have any questions or need assistance, our support team is here to help. 
+      We look forward to a successful partnership in maintaining law and order in your 
+      community.
+    </p>
+
+    <p>
+      Thank you for choosing the FIRNow. Welcome aboard!
+    </p>
+  </body>
+</html>
+"""
+
+welcome_text = """\
+Welcome to the FIR Management System!
+
+Your police station is now registered, and you have entered a new era of streamlined FIR management. With our user-friendly system, you can efficiently handle, store, and track FIRs, ensuring a more organized and responsive approach to law enforcement.
+
+If you have any questions or need assistance, our support team is here to help. We look forward to a successful partnership in maintaining law and order in your community.
+
+Thank you for choosing the FIR Management System. Welcome aboard!
+"""
