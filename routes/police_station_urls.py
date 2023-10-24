8caceec0ce7dc2c3e2b0d9fc7184f54c33245a41
Tsubasa <scientistjishan@gmail.com>
2023-10-24 20:58:21 +0530

    Added url for reset password, and renamed few variables.

--- routes/police_station_urls.py
+++ routes/police_station_urls.py
@@ -4,10 +4,12 @@ from config import settings
 
 prefix = "police-station"
 
-HOST: HttpUrl = settings.APP_HOST
-LOGIN_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/login")
-REGISTER_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/register")
-REFRESH_TOKEN_URL: HttpUrl = HttpUrl(f"{HOST}auth/refresh")
-SEND_OTP_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/send-otp")
-VERIFY_EMAIL_URL: HttpUrl = HttpUrl(f"{HOST}auth/{prefix}/verify-email")
-POLICE_STATION_DASHBOARD_URL: HttpUrl = HttpUrl(f"{HOST}{prefix}/dashboard")
+APP_HOST: HttpUrl = settings.APP_HOST
+API_HOST: HttpUrl = settings.API_HOST
+LOGIN_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/login")
+REGISTER_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/register")
+REFRESH_TOKEN_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/refresh")
+SEND_OTP_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/send-otp")
+VERIFY_EMAIL_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/verify-email")
+POLICE_STATION_DASHBOARD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/dashboard")
+RESET_PASSWORD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/reset-password")
