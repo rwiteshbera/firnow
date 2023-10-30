4b5124f90406030190ac25a636bedd761d8a2787
Tsubasa <scientistjishan@gmail.com>
2023-10-30 16:54:27 +0530

    Removed initialization code from session as different services have different requirements.

--- session.py
+++ session.py
@@ -1,12 +1,6 @@
-from contextlib import asynccontextmanager
 from typing import Optional
 
 from aiohttp import ClientSession
-from fastapi import FastAPI
-
-from databases.postgres import PostgresSession
-from databases.redis import RedisClient
-from utils.mail import Mailer
 
 
 class SingletonSession:
@@ -23,16 +17,3 @@ class SingletonSession:
         if cls._session is not None:
             await cls._session.close()
             cls._session = None
-
-
-@asynccontextmanager
-async def init(app: FastAPI):
-    SingletonSession.get_session()
-    await RedisClient.get_client()
-    await PostgresSession.init()
-    await Mailer.get_client()
-    yield
-    await SingletonSession.close_session()
-    await PostgresSession.close()
-    await RedisClient.close_client()
-    await Mailer.close_client()
