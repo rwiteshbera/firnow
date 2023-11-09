fe2420ef0e55265565812becc23bfcc9f487ba2c
Tsubasa <scientistjishan@gmail.com>
2023-11-09 11:13:37 +0530

    Changed exception order.

--- dependencies/upload.py
+++ dependencies/upload.py
@@ -22,7 +22,7 @@ async def get_file(request: Request) -> TemporaryUploadFile:
 
         return file
 
-    except ClientDisconnect:
+    except InvalidFileException:
         raise HTTPException(
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail={
@@ -38,7 +38,7 @@ async def get_file(request: Request) -> TemporaryUploadFile:
             },
         )
 
-    except InvalidFileException:
+    except ClientDisconnect:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail={
