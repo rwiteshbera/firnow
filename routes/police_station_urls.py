5c644f0a17c40624684c09c820a05294256ed46a
J16N <jishan2001bhattacharya@gmail.com>
2024-05-22 17:35:54 +0530

    Changed urls depending on development or production mode.

--- routes/police_station_urls.py
+++ routes/police_station_urls.py
@@ -1,15 +1,26 @@
 from pydantic import HttpUrl
 
-from config import settings
+from config import Mode, settings
 
 prefix = "police-station"
 
 APP_HOST: HttpUrl = settings.APP_HOST
 API_HOST: HttpUrl = settings.API_HOST
-LOGIN_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/login")
-REGISTER_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/register")
-REFRESH_TOKEN_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/refresh")
-SEND_OTP_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/send-otp")
-VERIFY_EMAIL_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/verify-email")
-POLICE_STATION_DASHBOARD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/dashboard")
-RESET_PASSWORD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/reset-password")
+
+if settings.MODE == Mode.DEV:
+    LOGIN_URL: HttpUrl = HttpUrl(f"{API_HOST}{prefix}/login")
+    REGISTER_URL: HttpUrl = HttpUrl(f"{API_HOST}{prefix}/register")
+    REFRESH_TOKEN_URL: HttpUrl = HttpUrl(f"{API_HOST}refresh")
+    SEND_OTP_URL: HttpUrl = HttpUrl(f"{API_HOST}{prefix}/send-otp")
+    VERIFY_EMAIL_URL: HttpUrl = HttpUrl(f"{API_HOST}{prefix}/verify-email")
+    POLICE_STATION_DASHBOARD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/dashboard")
+    RESET_PASSWORD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/reset-password")
+
+else:
+    LOGIN_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/login")
+    REGISTER_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/register")
+    REFRESH_TOKEN_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/refresh")
+    SEND_OTP_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/send-otp")
+    VERIFY_EMAIL_URL: HttpUrl = HttpUrl(f"{API_HOST}auth/{prefix}/verify-email")
+    POLICE_STATION_DASHBOARD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/dashboard")
+    RESET_PASSWORD_URL: HttpUrl = HttpUrl(f"{APP_HOST}{prefix}/reset-password")
