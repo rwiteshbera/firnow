e711f1b33c5d5b4863b0ad101f10ecccec51a10d
J16N <jishan2001bhattacharya@gmail.com>
2024-05-29 11:14:06 +0530

    Added CORS

--- services/auth.py
+++ services/auth.py
@@ -41,6 +41,8 @@ auth_service.add_middleware(
     allow_origins=[
         "http://127.0.0.1:8080",
         "http://127.0.0.1:3000",
+        "http://localhost:8080",
+        "http://localhost:3000",
         "https://api.firnow.duckdns.org",
         "http://api.firnow.duckdns.org",
     ],
