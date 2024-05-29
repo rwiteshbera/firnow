eb3669481941cba18eaeb2447bb091ac993c5004
priyanka kothari <priyankakothari751@gmail.com>
2024-05-29 17:06:50 +0530

    changes in login.jsx

--- src/pages/police-station/Login.jsx
+++ src/pages/police-station/Login.jsx
@@ -1,44 +1,68 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorOpen, setErrorOpen] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [validationOpen, setValidationOpen] = useState(false);
+  const [validationMessage, setValidationMessage] = useState("");
 
   const handleLogin = async (e) => {
     e.preventDefault();
 
     const formData = {
       username: email,
-      password: password
+      password: password,
     };
 
     try {
-      const resp = await fetch(`http://127.0.0.1:8000/police-station/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams(formData).toString()
-      });
+      console.log("Hello");
+      console.log(email);
+      console.log(password);
+      const resp = await fetch(
+        `http://127.0.0.1:8000/police-station/login`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: new URLSearchParams(formData).toString()
+        }
+      );
 
-      if (resp.ok) {
-
+      if (resp.status === 401) {
+        const errorData = await resp.json();
+        setErrorMessage(errorData.message || "Invalid Credentials");
+        setErrorOpen(true);
+      } else if (resp.status === 422) {
+        const errorData = await resp.json();
+        setValidationMessage(errorData.message || "Validation error");
+        console.log(errorData.message);
+        setValidationOpen(true);
+      } else if (resp.status === 404) {
+        const errorData = await resp.json();
+        setValidationMessage(errorData.message || "Account retrival error");
+        console.log(errorData.message);
+        setValidationOpen(true);
+      } else if (resp.ok) {
         const data = await resp.json();
         const accessToken = data.accessToken;
         console.log("login successful");
         console.log(accessToken);
         if (typeof window !== "undefined") {
-          localStorage.setItem('accessToken', data.accessToken);
+          localStorage.setItem("accessToken", data.accessToken);
         }
 
-        window.location.hrf = `/police-station/dashboard`;
+        window.location.href = `/police-station/dashboard`;
       } else {
         // Handle error response
-        console.error('Login failed');
+        console.error("Login failed");
       }
     } catch (error) {
-      console.error('Error logging in:', error);
+      console.error("Error logging in:", error);
     }
   };
 
@@ -47,7 +71,7 @@ export default function Login() {
       <h2 className="form_title2">Log in</h2>
       <div className="add-container">
         <div className="form_lodge2">
-          <form onSubmit={handleLogin} >
+          <form onSubmit={handleLogin}>
             <div className="input-container">
               <h5 className="input-header">Mail id</h5>
               <input
@@ -65,46 +89,20 @@ export default function Login() {
                 placeholder="Enter the password here"
               ></input>
             </div>
-
-            {/* <div className="input-container">
-            <h5 className="input-header">District</h5>
-            <input
-              className="input"
-              type=""
-              placeholder="Enter the district here"
-            ></input>
-          </div>
-
-          <div className="input-container">
-            <h5 className="input-header">Name</h5>
-            <input
-              className="input"
-              type="text"
-              placeholder="Enter the name of police station here"
-            ></input>
-          </div> */}
-
-            {/* <div className="input-container">
-                         <div className="save-btn login_btn">
-                              <input type="button" value="LOG IN"></input>
-                         </div>
-
-                   </div> */}
             <div className="input-container">
-              <div className="submit_div" style={{ width: '60%' }}>
+              <div className="submit_div" style={{ width: "60%" }}>
                 <button type="submit" className="submit_btn">
                   Login
                 </button>
               </div>
-
             </div>
           </form>
-          <Link to='/police-station/register'>
+          <Link to="/police-station/register">
             <u>Register here</u>
           </Link>
         </div>
       </div>
-      <div style={{ padding: '40px' }} />
+      <div style={{ padding: "40px" }} />
     </>
   );
 }
